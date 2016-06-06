d52d364e107295eb4124a8cf02f98138a64b4492
Jérémie Leclerc <le.microarchitechte@gmail.com>
2016-06-06 23:19:31 +0200

    Sync.*

--- src/com/ensicaen/awale/fxml/RulesLayoutController.java
+++ src/com/ensicaen/awale/fxml/RulesLayoutController.java
@@ -39,8 +39,8 @@ public class RulesLayoutController implements Initializable {
 
     public void handleRules(ActionEvent event) {
         txtRules.setText("Règle 1 : Seulement deux joueurs peuvent s'affronter. \n"
-                + "\nRègle 2 : On répartit quarante-huit graines dans les douze trous à raison de quatre graines par trou.\n"
-                + "\nRègle 3 : Chaque joueur joue à tour de rôle, celui qui joue en premier est tiré au hasard. Le joueur va prendre l'ensemble des graines dans l'un des trous de son territoire et les distribuer, un par trou, dans le sens inverse des aiguilles d'une montre.\n"
+                + "\nRègle 2 : Forty-eight seeds are distributed in the twelve holes with four seeds per hole.\n"
+                + "\nRègle 3 : Each player plays in turn, the one that goes first is drawn at random. The player will take all the seeds in one of the holes of its territory and distribute them, by a hole in the opposite direction clockwise.\n"
                 + "\nRègle 4 : Si sa dernière graine tombe dans un trou du camp adverse et qu'il y a maintenant deux ou trois graines dans ce trou, le joueur récupère ces deux ou trois graines et les met de côté. Ensuite il regarde la case précédente : si elle est dans le camp adverse et contient deux ou trois graines, il récupère ces graines, et ainsi de suite jusqu'à ce qu'il arrive à son camp ou jusqu'à ce qu'il y ait un nombre de graines différent de deux ou trois.\n"
                 + "\nRègle 5 : On ne saute pas de case lorsqu'on égrène sauf lorsqu'on a douze graines ou plus, c'est-à-dire qu'on fait un tour complet : on ne remplit pas la case dans laquelle on vient de prendre les graines.\n"
                 + "\nRègle 6 : Il faut «nourrir » l'adversaire, c'est-à-dire que, quand celui-ci n'a plus de graines, il faut absolument jouer un coup qui lui permette de rejouer ensuite. Si ce n'est pas possible, la partie s'arrête et le joueur qui allait jouer capture les graines restantes.\n"
